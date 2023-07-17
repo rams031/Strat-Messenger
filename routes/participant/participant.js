1ee7409a93f5c7255af307d7b3bdb8436f08c00e
Jacob <pauljacobalcaraz@gmail.com>
2023-07-17 11:32:43 +0800

    added chats

--- routes/participant/participant.js
+++ routes/participant/participant.js
@@ -5,14 +5,23 @@ const ParticipantModel = require("./../../model/participant/participant");
 const auth = require("./../../middleware/auth");
 const { view } = require("./../../action/action");
 
-router.get("/", auth, (req, res, next) => {
-  const getAllParticipant = view(ParticipantModel, res, next);
-  return getAllParticipant;
+router.get("/chats", auth, async (req, res, next) => {
+	const { _id: userId } = req.user;
+
+	const getAllParticipant = await ParticipantModel.find({
+		userId: userId,
+	})
+		.populate({
+			path: "roomId",
+			select: ["roomName"],
+		})
+		.exec();
+	console.log(getAllParticipant);
+	return res.send(getAllParticipant);
 });
 
 router.get("/", auth, (req, res, next) => {
-    
-    const getAllParticipant = view(ParticipantModel, res, next);
-    return getAllParticipant;
-  });
+	const getAllParticipant = view(ParticipantModel, res, next);
+	return getAllParticipant;
+});
 module.exports = router;
--- routes/room/room.js
+++ routes/room/room.js
@@ -7,38 +7,36 @@ const { create, view, login } = require("./../../action/action");
 const UserModel = require("../../model/user/user");
 
 router.get("/", (req, res, next) => {
-  console.log("working");
-  res.status(200).send({ message: "Testing Water for Room" });
+	console.log("working");
+	res.status(200).send({ message: "Testing Water for Room" });
 });
 
 router.post("/createroom", auth, async (req, res, next) => {
-  const { roomName, participants, user } = req.body;
-  const { _id: userId } = req.user;
+	const { roomName, participants, user } = req.body;
+	const { _id: userId } = req.user;
 
-  await RoomModel.create({ ...roomName })
-    .then(async (response) => {
-      const { _id } = response || {};
-      if (!_id) return res.status(400).send({ message: "Having Problem" });
+	await RoomModel.create({ roomName })
+		.then(async (response) => {
+			const { _id } = response || {};
+			if (!_id) return res.status(400).send({ message: "Having Problem" });
 
-      const convertedParticipants = [userId.toString(), ...participants];
+			const convertedParticipants = [userId.toString(), ...participants];
 
-      const listOfParticipant = convertedParticipants.map((participant) => {
-        return {
-          userId: participant,
-          roomId: _id.toString(),
-          userType: "Admin",
-        };
-      });
+			const listOfParticipant = convertedParticipants.map((participant) => {
+				return {
+					userId: participant,
+					roomId: _id.toString(),
+					userType: "Admin",
+				};
+			});
 
-      await ParticipantModel.insertMany(listOfParticipant).then(
-        (result) => {
-          res.status(201).send(result);
-        }
-      );
-    })
-    .catch((err) => {
-      return next(err);
-    });
+			await ParticipantModel.insertMany(listOfParticipant).then((result) => {
+				res.status(201).send(result);
+			});
+		})
+		.catch((err) => {
+			return next(err);
+		});
 });
 
 module.exports = router;
